b7d9a2cc2e976c7c9ec8f195019fbd463e03d4de
CapSens-SM <contact@capsens.eu>
2021-12-26 03:14:56 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,88 @@
-import Head from 'next/head'
+import { useState } from "react";
+import dynamic from "next/dynamic";
+const ReCAPTCHA = dynamic(() => import("react-google-recaptcha"));
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const initialFormContent = {
+    firstName: "",
+    email: "",
+    captcha: ""
+  }
+  const [formContent, setFormContent] = useState(initialFormContent);
+  const [recaptchaNeeded, setRecaptchaNeeded] = useState(false);
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+  const handleChange = (e) => {
+    const target = e.target;
+    const inputName = target.name;
+    const authorizedField = Object.keys(initialFormContent);
+
+    if (!authorizedField.includes(inputName)) {
+      return;
+    }
+
+    const value = target.value;
+    setFormContent({ ...formContent, [inputName]: value });
+    setRecaptchaNeeded(true);
+  };
+
+  const recaptchaPublicKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
+  const onReCAPTCHAChange = async (captchaCode) => {
+    if (!captchaCode) {
+      return;
+    }
+
+    setFormContent({ ...formContent, captcha: captchaCode });
+  };
+
+  return (
+    <main className="bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 min-h-screen flex items-center justify-center">
+      <div className="max-w-5xl bg-slate-300 border border-slate-500 rounded-md p-8">
+        <h1 className="text-4xl font-bold text-center">
+          Optimizing ReCaptcha
+          <br />
+          <span className="text-indigo-600">with NextJs</span>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+        
+        <form className="flex flex-col gap-y-4 mt-6">
+          <input
+            id="firstName"
+            type="text"
+            name="firstName"
+            onChange={handleChange}
+            value={formContent.firstName}
+            placeholder="Fistname"
+            required={true}
+            className="bg-white rounded-md border border-slate-600 h-10 px-2"
+          />
+          <input
+            id="email"
+            type="text"
+            name="email"
+            onChange={handleChange}
+            value={formContent.email}
+            placeholder="Email"
+            required={true}
+            className="bg-white rounded-md border border-slate-600 h-10 px-2"
+          />
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          <div className="flex justify-center">
+            {recaptchaNeeded && <ReCAPTCHA
+              sitekey={recaptchaPublicKey}
+              onChange={onReCAPTCHAChange}
+              theme="dark"
+            />}
+          </div>
+
+          <button
+            type="submit"
+            className="w-full inline-flex items-center justify-center px-6 py-2 bg-slate-800 text-white rounded-md duration-200 disabled:cursor-not-allowed disabled:opacity-50"
+            disabled={formContent.captcha === ""}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+            Submit
+          </button>
+        </form>
+      </div>
+    </main>
   )
 }
